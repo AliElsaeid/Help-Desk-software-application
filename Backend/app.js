646deb724eee2539d9245d3e84ef90e20098db7b
AliElsaeid <120056055+AliElsaeid@users.noreply.github.com>
2023-11-21 21:19:09 +0200

    Initial commit

---
+++ Backend/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next()
+});
+
+
+const db_name = "Help_Desk";
+// * Cloud Connection
+// const db_url = `mongodb+srv://TestUser:TestPassword@cluster0.lfqod.mongodb.net/${db_name}?retryWrites=true&w=majority`;
+// * Local connection
+const db_url = `mongodb://127.0.0.1:27017/${db_name}`; // if it gives error try to change the localhost to 127.0.0.1
+
+// ! Mongoose Driver Connection
+
+const connectionOptions = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+};
+
+mongoose
+  .connect(db_url, connectionOptions)
+  .then(() => console.log("mongoDB connected"))
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.use(function (req, res, next) {
+  return res.status(404).send("404");
+});
+app.listen(3000, () => console.log("server started"));
+
