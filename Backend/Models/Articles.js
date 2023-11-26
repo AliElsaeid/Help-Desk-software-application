54c974029d9fab8fa1e5827d005b063a139e9ac2
AliElsaeid <120056055+AliElsaeid@users.noreply.github.com>
2023-11-26 19:48:23 +0200

    Edits

--- Backend/Models/Articles.js
+++ Backend/Models/Articles.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
-const knowledgeBaseSchema = new mongoose.Schema({
+const ArticleSchema = new mongoose.Schema({
   Title: { type: String, required: true },
+  Type: { type: String, required: true},
   Content: { type: String, required: true },
   Category: { type: String, required: true },
   AdministratorID: { type: mongoose.Schema.Types.ObjectId, ref: 'Users' },
 });
 
-const KnowledgeBaseModel = mongoose.model('KnowledgeBase', knowledgeBaseSchema);
+const ArticleModel = mongoose.model('Articles', ArticleSchema);
 
-module.exports = KnowledgeBaseModel;
+module.exports = ArticleModel;
