54360f1db0e7faa4a522749914fe479c18293f45
Nourallahh <nour.ehab.sharaf@gmail.com>
2023-12-02 23:07:30 +0200

    nour2

--- Backend/Routes/reportRoutes.js
+++ Backend/Routes/reportRoutes.js
@@ -1,37 +1,35 @@
 const express = require('express');
 const router = express.Router();
-const Ratings2 = require('../models/Rating');
-const User = require('./UserModel');
-const Ticket = require('./TicketModel');
-const Ratings = require('./RatingModel');
+const Ratings = require('../Models/RatingModel');
+const User = require('../UserModel');
+const Ticket = require('../TicketModel');
 
-//GET method to retrieve the user ratings
-router.get('/getUserRatings', async (req, res) => {
+//Create get request to retrieve ratings for the admin
+router.get('/ratings', async (req, res) => {
     try {
-// 1-Make sure that the user making the request is an admin
-      const isAdmin = req.user.role === 'admin';
+        const { userId } = req.body;
   
-      if (!isAdmin) {
-        return res.status(403).json({ error: 'Access denied. Admin permissions required.' });
-      }
-
-// 2-Make sure that the user has made ratings
-      const username = req.user.username;
-      
-      const userRatings = await Ratings.find({ 'user.username': username });
+ // 1-Find the user by username
+      const user = await User.findUser(userId);
   
-      if (userRatings.length === 0) {
-        return res.status(404).json({ error: 'User has not made any ratings.' });
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
       }
   
-    //Fetch ratings and populate the user to get the user details
-      const usernamesWithRatings = await Ratings.find({ 'user.username': username }).populate('user', 'username');
+ // 2-Make sure that the the user's role is admin
+      if (user.role === 'admin') {
+ // 3-If true then retrieve the attributes of the ratings table
+ // --> contains the attributes tickets.schema for the additional wanted attributes 
+        const ratings = await Ratings.find();
   
-      res.status(200).json(usernamesWithRatings);
+ // 4-Send the ratings as a response
+        return res.status(200).json({ ratings });
+      } else {
+        return res.status(403).json({ message: 'Access Denied. User is not an admin.' });
+      }
     } catch (error) {
       console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
+      return res.status(500).json({ message: 'Internal server error' });
     }
   });
   
-  module.exports = router;
