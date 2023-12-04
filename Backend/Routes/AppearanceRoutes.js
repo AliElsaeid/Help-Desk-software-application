770ac6953bd7e6e166c0edcc83d6adb6d5ac9d97
Omarhosny366 <omarhosny120@gmail.com>
2023-12-04 19:08:25 +0200

    authorization for appearance access

--- Backend/Routes/AppearanceRoutes.js
+++ Backend/Routes/AppearanceRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const AppearanceSetting = require('../Models/AppearanceSetting'); // Adjust the path based on your project structure
+const authorizationMiddleware = require('../Middleware/authorizationMiddleware');
 
 // API endpoint for updating appearance settings
-router.put('/appearance', async (req, res) => {
+router.put('/appearance', authorizationMiddleware(['admin']), async (req, res) => {
     try {
         // Check if the appearance settings record exists, if not, create a new one
         let appearanceSettings = await AppearanceSetting.findOne();
@@ -26,7 +27,7 @@ router.put('/appearance', async (req, res) => {
 });
 
 // API endpoint for retrieving current appearance settings
-router.get('/appearance', async (req, res) => {
+router.get('/appearance', authorizationMiddleware(['admin']), async (req, res) => {
     try {
         // Retrieve the appearance settings record
         const appearanceSettings = await AppearanceSetting.findOne();
