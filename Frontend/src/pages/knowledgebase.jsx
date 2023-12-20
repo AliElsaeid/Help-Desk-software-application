6e0ddeb6e3cfe14524b596c62bafafbd1785f15a
mahmoudmehany <mmehany5@gmail.com>
2023-12-20 23:04:26 +0200

    articles

--- Frontend/src/pages/knowledgebase.jsx
+++ Frontend/src/pages/knowledgebase.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import '../stylesheets/knowledgebasestyle.css';
-import Nav from 'react-bootstrap/Nav';
-
 
 const ArticlesList = () => {
   const [articles, setArticles] = useState([]);
